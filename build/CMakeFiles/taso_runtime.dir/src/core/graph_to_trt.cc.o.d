c70d4cb96a8ad47779ba44d4efa0491a7442f0ec
Ashwin Venkatram <ashwin@ip-172-31-27-159.us-east-2.compute.internal>
2022-11-24 05:54:50 +0000

    changed setup.py

---
+++ build/CMakeFiles/taso_runtime.dir/src/core/graph_to_trt.cc.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/taso_runtime.dir/src/core/graph_to_trt.cc.o: \
+ /home/ashwin/TASO_KERNEL_MAPPER/src/core/graph_to_trt.cc \
+ /usr/include/stdc-predef.h
--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Copyright 2019 Stanford
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
